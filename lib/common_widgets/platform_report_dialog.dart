e9f3c3eac0c23106d74f2e24a593f45b6b401493
Yacine-Benali <benalidev@gmail.com>
2021-03-01 23:01:51 +0100

    oupsi

--- lib/common_widgets/platform_report_dialog.dart
+++ lib/common_widgets/platform_report_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:alhalaqat/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:alhalaqat/common_widgets/platform_widget.dart';
 import 'package:flutter/cupertino.dart';
